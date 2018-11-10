32a5a52d5cb21eeff7d2c3d1a3c574705f3a92d7
viveksharmaui <sharma_vivek62@yahoo.com>
2018-11-11 01:07:33 +0500

    max-memory-restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps : [{
-    name      : 'API',
-    script    : 'server.js',
-    instances : 'max',
-    exec_mode : "cluster",
-    kill_timeout : 1000,
-    max_memory_restart : "512M",
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production : {
-      PORT:process.env.PORT,
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: "API",
+      script: "server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      kill_timeout: 1000,
+      max_memory_restart: "100M",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        PORT: process.env.PORT,
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ]
 };
